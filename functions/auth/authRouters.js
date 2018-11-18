418905ea1b30ea78051629330de8a5b4c4e4f892
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-17 21:54:20 -0800

    basic auth set up

--- functions/auth/authRouters.js
+++ functions/auth/authRouters.js
@@ -1,5 +1,21 @@
 const express = require('express');
-const admin = require('../firebase-admin.js');
+const { auth } = require('../firebase-admin.js');
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const token = req.headers.authorization;
+  auth()
+    .verifyIdToken(token)
+    .then(async decodedToken => {
+      const uid = decodedToken.uid;
+      req.body.uid = uid;
+      await auth().setCustomClaims(uid, { subTier: '0' });
+      const customToken = await auth().createCustomToken(uid, { subTier: '0' });
+      res.json({ customToken }).redirect('/forms');
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
