091fd1723d47bd30962cce5aac25080762a5282c
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-17 20:28:48 -0800

    Routers connected to server

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,9 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const userRouters = require('./users/userRouters.js');
+const authRouters = require('./auth/authRouters.js');
+const server = express();
 
-module.exports = server
+server.use('/api/user', userRouters);
+server.use('/api/auth', authRouters);
+
+module.exports = server;
