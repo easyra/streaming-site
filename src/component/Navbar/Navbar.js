6bbbfe721e1e1c7dbe7feca8f61d4dea1420d0e6
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-17 20:07:28 -0800

    firebase functions init'd

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default (Navbar = props => {
+  return <header>
+    <nav>
+      <h1>MockStreamer</h1>
+      <div>
+        
+      </div>
+    </nav>
+  </header>;
+});
