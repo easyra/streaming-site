8af24193ecdd3d424c32a9a06236b9c58ffbd24c
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-20 14:11:02 -0800

    removed appContainer component

--- src/component/AppContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar/Navbar';
-
-export default class AppContainer extends Component {
-  state = {};
-  render() {
-    return <Navbar />;
-  }
-}
