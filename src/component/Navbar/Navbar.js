c5bfc90abd2980fe06412440d825a2ca0e37ccce
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-19 18:29:30 -0800

    fixed bug with material-ui

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,50 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
-const Navbar = props => {
-  return (
-    <nav class="blue darken-2 navbar-fixed">
-      <div className="nav-wrapper">
-        <div className="container">
-          <a className="brand-logo left">MOCKSTREAMER</a>
-          <ul className="right">
-            <li>
-              {/* Social Media Dropdown Button */}
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  handleClick = () => {
+    console.log(this.myRef.current.dropdown());
+    //this.myRef.current;
+  };
+  render() {
+    return (
+      <nav class="blue darken-2 navbar-fixed">
+        <div className="nav-wrapper">
+          <div className="container">
+            <a className="brand-logo left">MOCKSTREAMER</a>
+            <ul className="right">
               <li>
-                <a
-                  class="dropdown-trigger"
-                  href="#!"
-                  data-target="social"
-                  onClick={handleClick}
-                >
-                  Follow Me<i class="material-icons right">arrow_drop_down</i>
-                </a>
+                {/* Social Media Dropdown Button */}
+                <li>
+                  <a
+                    class="dropdown-trigger"
+                    href="#!"
+                    data-target="social"
+                    ref={this.myRef}
+                    onClick={this.handleClick}
+                  >
+                    Follow Me<i class="material-icons right">arrow_drop_down</i>
+                  </a>
+                </li>
+                {/* Dropdown Content */}
+                <ul id="social" class="dropdown-content">
+                  <li>
+                    <a href="#!">one</a>
+                  </li>
+                  <li>
+                    <a href="#!">two</a>
+                  </li>
+                  <li class="divider" />
+                  <li>
+                    <a href="#!">three</a>
+                  </li>
+                </ul>
+                {/* Login Button */}
+
+                <Button variant="contained" color="primary">
+                  LOGIN
+                </Button>
               </li>
-              {/* Dropdown Content */}
-              <ul id="social" class="dropdown-content">
-                <li>
-                  <a href="#!">one</a>
-                </li>
-                <li>
-                  <a href="#!">two</a>
-                </li>
-                <li class="divider" />
-                <li>
-                  <a href="#!">three</a>
-                </li>
-              </ul>
-              {/* Login Button */}
-              <a class="waves-effect waves-light btn red">LOGIN</a>
-            </li>
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
-
-const handleClick = event => {
-  console.log('hi');
-  event.currentTarget.open();
-};
-
-export default Navbar;
+      </nav>
+    );
+  }
+}
